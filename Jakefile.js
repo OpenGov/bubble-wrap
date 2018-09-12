fbda30a17cf05433b7baf4bcb73562f1c4763ab2
Jason McInerney <jmcinerney@opengov.com>
2018-09-11 22:24:06 -0700

    First commit with ci, along with linting and testing

---
+++ Jakefile.js
@@ -0,0 +1,47 @@
+/*global desc, task, jake, fail, complete */
+"use strict";
+
+desc("Build and test");
+task("default", ["lint", "test"]);
+
+desc("Lint all the things");
+task("lint", [], function() {
+    var lint = require("./build/lint/lint_runner.js");
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+    files.exclude("test");
+
+    var passed = lint.validateFileList(files.toArray(), nodeLintOptions(), {} );
+    if (!passed) fail("Lint failed");
+});
+
+desc("Test everything");
+task("test", [], function() {
+    var Mocha = require("mocha");
+    var mocha = new Mocha({reporter: 'min'});
+    mocha.addFile('./test/sample.js');
+    mocha.run(function(failures) {
+        if (failures) fail("Tests Failed");
+    });
+});
+
+function nodeLintOptions() {
+    return {
+        bitwise: true,
+        curly: false,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: false,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        regexp: true,
+        undef: true,
+        strict: true,
+        trailing: true,
+        node: true
+    };
+}
---
+++ todo.md
@@ -0,0 +1,61 @@
+# Bubble-Wrap
+
+Here are some MVPs to work on 
+- Create reports for test coverage
+- Create reports for test results
+
+## Installation of tools
+
+### Mocha
+
+`npm install --global mocha`
+`npm install --save-dev mocha` 
+
+### Jake
+
+`npm install jake`
+
+also 
+
+`npm install jshint` for all our linting needs
+
+Now create a jake file with the first CI activies. 
+
+### Copy over lint_runner.js 
+Copy from the james shore project
+about Test Driven Javascript
+
+
+### Installing a CI system
+
+- Circle CI
+
+### D3
+
+* Installing D3
+
+`npm install d3`
+
+NOTE: This is optional and also a reference of the tools i may 
+Want to install in the future. 
+
+* Hexbin, a D3 Plugin
+
+`npm install d3-hexbin`
+
+MVP
+- circle packing shows all acceptance test scenarios
+
+Stories
+- Create continuous integration environment
+- Write the first unit test
+- Create a json formatter or converter of all test scenarios
+- Ability to merge one json file into another 
+- Create a structure to support multiple browsers
+- 
+
+Spikes
+- how to handle color and size of circles
+- take an existing formatter and convert it to 
+    the json format for d3
+- 
